02eeaa5412fa36ea7b46254690c9a713bfb111d5
Daijiro Tsushima <daijiro.tsushima@gmail.com>
2023-10-18 14:17:21 -0400

    Initial Commit

---
+++ Lead With Power Story/Lead_With_Power_StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lead_With_Power_StoryApp.swift
+//  Lead With Power Story
+//
+//  Created by Daijiro Tsushima on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Lead_With_Power_StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
